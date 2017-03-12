e54dab9dae5f9ab9a10477077d7e3b614fc5ec70
Milton <miltomcamara@gmail.com>
2017-03-12 00:53:26 -0300

    Initial Project

---
+++ src/TagHelpers/TagHelpers/EmailHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelpers.TagHelpers
+{
+    [HtmlTargetElement("phone")]
+    public class EmailHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.Add("href", $"mailto:{output.GetChildContentAsync().Result.GetContent()}");
+            output.Content.SetContent(output.GetChildContentAsync().Result.GetContent());
+        }
+    }
+}
---
+++ src/TagHelpers/TagHelpers/PhoneHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelpers.TagHelpers
+{
+    [HtmlTargetElement("phone")]
+    public class PhoneHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string formatedPhone = String.Format("{0:(##) #####-####}", double.Parse(output.GetChildContentAsync().Result.GetContent()));
+            output.TagName = "a";
+            output.Attributes.Add("href", $"tel:{output.GetChildContentAsync().Result.GetContent()}");
+            output.Content.SetContent(formatedPhone);
+        }
+    }
+}
